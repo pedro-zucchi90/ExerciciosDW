43a64b97a7912db9bf7bc5e1d0c1107beb7cd8ff
Pedro Zucchi <predozucchi@gmail.com>
2025-04-01 21:19:54 -0300

    atualizado

---
+++ main.py
@@ -0,0 +1,4 @@
+from routes import app  # Apenas importamos `app` do `routes.py`
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- routes.py
+++ routes.py
@@ -3,52 +3,16 @@ import uuid
 
 app = Flask(__name__)
 
-# @app.route("/", methods = ["GET"] )
-# def HelloWorld():
-#     return "Hello World"
-#-----------------------------------------------------
-# alunos = [
-#         {
-#             'id': 1, 'nome': 'Julio'
-#         },
-#         {
-#            'id': 2, 'nome': 'Pietra'
-#         },
-#         {
-#            'id': 3, 'nome': 'Davi'
-#         }
-#     ]
-
-# @app.route("/alunos/<int:id>", methods=["GET"])
-# def metodoGet_por_id(id):
-#     for aluno in alunos:
-#         if aluno['id'] == id:
-#             return aluno
-#     return{}
-    
-# @app.route("/alunos", methods=["GET"])
-# def metodoGet():
-#     return alunos
-
-# @app.route("/alunos/", methods=["POST"])
-# def metodoPost():
-#     dados_recebidos = request.get_json()
-#     novo_id = len(alunos)+1
-#     alunos.append({"id": novo_id, "nome": dados_recebidos["nome"]})
-#     return {"mensagem": "OK"}
-#-----------------------------------------------------
-
-
 calculos = []
 
-@app.route("/soma", methods = ["POST"])
+@app.route("/soma", methods=["POST"])
 def soma():
     dados_recebido = request.get_json()
     num1 = float(dados_recebido['num1'])
     num2 = float(dados_recebido['num2'])
     resultado = num1 + num2
     calculos.append(
-        {    
+        {
             "id": str(uuid.uuid4()),
             "num1": num1,
             "num2": num2,
@@ -57,11 +21,11 @@ def soma():
     )
     return {'resultado': resultado}
 
-@app.route("/calculos", methods = ["GET"])
+@app.route("/calculos", methods=["GET"])
 def mostrarSomas():
     return calculos
 
-@app.route("/deletar/<id>", methods=['DELETE'])
+@app.route("/deletar/<id>", methods=["DELETE"])
 def deletar(id):
     global calculos
     id_uuid = uuid.UUID(id)
@@ -79,7 +43,6 @@ def editar_item(id):
     
     id_uuid = uuid.UUID(id)
 
-    # atualizar o cálculo com o ID fornecido
     for calculo in calculos:
         if uuid.UUID(calculo['id']) == id_uuid:
             calculo['num1'] = num1
@@ -88,6 +51,3 @@ def editar_item(id):
             break
 
     return {"mensagem": "calculo editado com sucesso"}
-
-
-
